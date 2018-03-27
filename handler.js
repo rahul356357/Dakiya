da302dd671cc02b912c4ab2282b852fa58632c15
rahul Bora <rahul.bora@treebohotels.com>
2018-03-27 23:49:27 +0530

    after successfull operation

--- handler.js
+++ handler.js
@@ -3,11 +3,11 @@ const cheerio = require('cheerio');
 const request = require('request');
 const rp = require('request-promise');
 const MongoClient = require('mongodb').MongoClient
+var aws = require('aws-sdk');
 const transporter = require('nodemailer').createTransport({
   service: 'gmail',
   auth: {
     user: 'rahulbora2068@gmail.com',
-    pass: 'tanuja@2068'
   }
 });
 const MLAB_URL = 'mongodb://boris:boris@ds123499.mlab.com:23499/dakiya';
@@ -18,14 +18,14 @@ const COLLECTIONNAME = 'courier';
 module.exports.registerToDatabase = (event, context, callback) => {
   const body = JSON.parse(event.body);
   const id = body.id;
-  const receiverEmail = body.receiverEmail;
+  const rmail = body.rmail;
   const createResponse = (statusCode, message, event) => {
     return {
       statusCode: statusCode,
       body: JSON.stringify({message: message})
     }
   }
-  if (id === null || receiverEmail === null) {
+  if (id === null || rmail === null) {
     callback(null, createResponse(400, 'Required Field Missing'))
   }
   MongoClient.connect(MLAB_URL, (err, database) => {
@@ -35,15 +35,13 @@ module.exports.registerToDatabase = (event, context, callback) => {
     }
     const db = database.db(DBNAME);
     const collection = db.collection(COLLECTIONNAME);
-
     collection.update({
       courierId: id
     }, {
       courierId: id,
-      receiverEmail: receiverEmail
+      rmail: rmail
     }, {upsert: true}).then(d => {
-      console.log('db save result')
-      console.log(d);
+      console.log('db save result');
       callback(null, createResponse(200, 'Courier Successfully Registered'))
       database.close()
     }).catch(err => {
@@ -55,20 +53,22 @@ module.exports.registerToDatabase = (event, context, callback) => {
 }
 
 module.exports.crawlCourier = (event, context, callback) => {
-  const createResponse = (statusCode, message, event) => {
+
+  const createResponse = (statusCode, message, success) => {
     return {
       statusCode: statusCode,
-      body: JSON.stringify({message: message})
+      body: JSON.stringify({message: message, success: success})
     }
   }
 
   const body = JSON.parse(event.body);
   const id = body.id;
-  if (id === null) {
+  const rmail = body.rmail;
+  if (id === null || rmail === null) {
     callback(null, createResponse(400, 'Required Missing Field'))
   }
+
   MongoClient.connect(MLAB_URL, (err, database) => {
-    console.log('in herer in mongodb connection');
     if (err) {
       console.log('error in connection to database')
       callback(null, createResponse(500, 'Unable to connect to Database'))
@@ -80,6 +80,7 @@ module.exports.crawlCourier = (event, context, callback) => {
     const travellingData = [];
     const deliveryData = []
     let documentLink = ''
+  
     const options = {
       uri: `http://www.shreemaruticourier.com/track-your-shipment/?tracking_id=${id}`,
       transform: (body) => {
@@ -111,11 +112,6 @@ module.exports.crawlCourier = (event, context, callback) => {
           documentLink = a;
         }
       });
-      console.log(bookingData);
-      console.log(travellingData);
-      console.log(deliveryData);
-      console.log(documentLink)
-
       return {
         crawledData: bookingData[0].split(/\n/),
         link: documentLink,
@@ -124,32 +120,72 @@ module.exports.crawlCourier = (event, context, callback) => {
           message: travellingData[1]
         }
       }
-    }).then(data => {
-      console.log('in herer in collection');
-      collection.findOneAndUpdate({
-        courierId: id
-      }, {
-        courierId: id,
-        deliveryStatus: data.crawledData[7],
-        reciever: data.crawledData[4],
-        bookingDate: data.crawledData[1],
-        fromCenter: data.crawledData[2],
-        toCenter: data.crawledData[3],
-        courierType: data.crawledData[5],
-        deliveryDate: data.crawledData[6],
-        documentLink: data.link,
-        travellingInfo: data.travellingInfo,
-        time: new Date().toString(),
-        emailSend: false
-      }, {upsert: true}).then(d => {
-        console.log('in herereererer data saved')
-        callback(null, createResponse(200, 'successfully crawled'))
-        database.close()
-      }).catch(() => {
-        throw new Error('Unable to Save to Database');
-      })
+    }).then(data=> {
+      const deliveryRegex = new RegExp(/DELIVERED/);
+      if (deliveryRegex.test(data.crawledData[7])) {
+        console.log("inside success" , rmail);
+        collection.findOneAndUpdate({
+          courierId: id,
+          rmail: rmail
+        }, {
+          courierId: id,
+          rmail: rmail,
+          deliveryStatus: data.crawledData[7],
+          reciever: data.crawledData[4],
+          bookingDate: data.crawledData[1],
+          fromCenter: data.crawledData[2],
+          toCenter: data.crawledData[3],
+          courierType: data.crawledData[5],
+          deliveryDate: data.crawledData[6],
+          documentLink: data.link,
+          travellingInfo: data.travellingInfo,
+          time: new Date().toString(),
+          emailSend: true
+        }, {upsert: true}).then(successObject => {
+          const mailOptions = {
+            from: 'rahulbora2068@gmail.com', // sender address
+            to: `${rmail}`, // list of receivers
+            subject: 'Delivered', // Subject line
+            html: '<p>Your html here</p>' // plain text body
+          };
+            transporter
+              .sendMail(mailOptions,  (error, info) => {
+                if (error) {
+                  console.log('error in sending mail herer')
+                  throw new Error({success:false, message:'error in mailer'})
+                } else {
+                  return({success:true , message:"mail sent successfully"})
+                }
+              });
+          }).then(mailerResponse => {
+          return({success: true, message:"mail sent success"})
+        }).catch(err => {
+          console.log('errror in saving to database ###############')
+          throw new Error  ({success: false, message: "error in save database"})
+        })
+      } else {
+        collection.findOneAndUpdate({
+          courierId: id,
+          rmail: rmail
+        }, {
+          courierId: id,
+          rmail: rmail,
+          time: new Date().toString(),
+          emailSend: false
+        }, {upsert: true}).then(successObject => {
+          return ({success: true, message: 'saved successfully to database'})
+        }).catch(err => {
+          console.log('errror in saving to database ###############%%%%%%%%%%%%%%%%')
+          throw new Error({success: false, message: err})
+        })
+      }
+      }).then(response => {
+      callback(null, createResponse(200, "successfull opertaion"  , true))
+      database.close()
     }).catch(err => {
-      callback(null, createResponse(500, err))
+      console.log(err);
+      console.log('errror in saving to database')
+      callback(null, createResponse(500, "line 204 " , false ))
       database.close()
     })
   })
